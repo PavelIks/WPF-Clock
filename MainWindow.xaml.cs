f51dd538f3dc24e5d97ce7915e6f55e0c1d90406
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2020-07-31 21:39:55 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace Clock
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private DispatcherTimer timer;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1.0);
+            timer.Start();
+            timer.Tick += new EventHandler(delegate (object s, EventArgs a)
+            {
+                Tuch.Content = "" + DateTime.Now.Hour + ":" + DateTime.Now.Minute;
+            });
+        }
+    }
+}
